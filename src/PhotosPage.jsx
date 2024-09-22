314837e00d1fdee1e1fc82a94c8d221b103816e1
ImJustRob <robertalothian@hotmail.com>
2024-09-22 15:43:33 -0400

    add new/create actions

--- src/PhotosPage.jsx
+++ src/PhotosPage.jsx
@@ -1,6 +1,7 @@
  import axios from "axios";
  import { useState, useEffect } from "react";
   import { PhotosIndex } from "./PhotosIndex";
+  import { PhotosNew } from "./PhotosNew";
 
   export function PhotosPage() {
    const [photos, setPhotos] = useState([]);
@@ -13,10 +14,19 @@
      });
    };
 
+      const handleCreate = (params, successCallback) => {
+         console.log("handleCreate", params);
+         axios.post("http://localhost:3000/photos.json", params).then((response) => {
+           setPhotos([...photos, response.data]);
+           successCallback();
+         });
+       };
+
    useEffect(handleIndex, []);
 
     return (
       <main>
+        <PhotosNew onCreate={handleCreate} />
         <PhotosIndex photos={photos} />
       </main>
     );
