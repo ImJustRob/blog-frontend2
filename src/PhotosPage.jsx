ff373af04c5fc6e1282c45da8bac15fe907cbebb
ImJustRob <robertalothian@hotmail.com>
2024-09-22 15:37:28 -0400

    add index action

--- src/PhotosPage.jsx
+++ src/PhotosPage.jsx
@@ -1,7 +1,23 @@
-export function PhotosPage() {
+ import axios from "axios";
+ import { useState, useEffect } from "react";
+  import { PhotosIndex } from "./PhotosIndex";
+
+  export function PhotosPage() {
+   const [photos, setPhotos] = useState([]);
+
+   const handleIndex = () => {
+     console.log("handleIndex");
+     axios.get("http://localhost:3000/photos.json").then((response) => {
+       console.log(response.data);
+       setPhotos(response.data);
+     });
+   };
+
+   useEffect(handleIndex, []);
+
     return (
       <main>
-        <h1>Welcome to React!</h1>
+        <PhotosIndex photos={photos} />
       </main>
-    )
+    );
   }
